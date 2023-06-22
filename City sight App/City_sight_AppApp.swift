79198f23269084e613a76b403116b5106374b1c0
sannyralph <sannylauren@gmail.com>
2023-06-22 12:12:38 -0400

    Initial Commit

---
+++ City sight App/City_sight_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_sight_AppApp.swift
+//  City sight App
+//
+//  Created by SANNI ABDULRAFIU on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct City_sight_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
